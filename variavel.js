0cb7c6d2aa03e0c05451ffd334c7a427a0cc8fdf
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-03-12 09:30:41 -0300

    Create variavel.js

---
+++ variavel.js
@@ -0,0 +1,12 @@
+var respostaDeTudo = 42;
+let idade = 29;
+const pi = 3.14;
+
+{
+    var respostaDeTudo = 42;
+    let idade = 29;
+    const pi = 3.14;
+    console.log(respostaDeTudo, idade, pi);
+}
+
+console.log(respostaDeTudo, idade, pi);
